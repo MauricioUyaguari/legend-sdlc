638088b2b512b13c090a411f76b2f5ace06a05bd
Mauricio Uyaguari <lmauricio12@gmail.com>
2024-08-09 10:33:15 -0400

    Update testing.sh

--- pipeline-tests/testing.sh
+++ pipeline-tests/testing.sh
@@ -1,1 +1,7 @@
 sed 's/_suffix$//' data.txt | jq -R -s 'split("\n") | map(select(length > 0))' > output.json
+
+
+find /path/to/directory -type f -name "*.txt" -exec grep -v '^$' {} + | sed 's/_suffix$//' > combined.txt
+
+
+sed -n '10,20p' input.txt > output.txt
